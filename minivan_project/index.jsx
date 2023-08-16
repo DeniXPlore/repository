13f3eb4f77d0db05c5f62180bedea1a6b37a9a2c
Denis1982-11-23 <denisemelanov53635@gmail.com>
2023-08-16 22:43:06 +0500

    add index.html index.css index.jsx

---
+++ minivan_project/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDom from 'react-dom/client';
+
+function APP(){
+  return (
+    <h1></h1>
+  )
+}
+
+ReactDom.createRoot(document.getElementById('root')).render(<App />);
