024b94dd78c0164b1a795860fe196a20066ba27e
Denis1982-11-23 <denisemelanov53635@gmail.com>
2023-09-05 21:40:13 +0500

    make error handler

--- minivan_project/src/api.js
+++ minivan_project/src/api.js
@@ -1,5 +1,13 @@
 async function getVans() {
   const res = await fetch("/api/vans");
+  if (!res.ok) {
+    const errorMessage = {
+      message: "Failed to fetch vans",
+      statusText: res.statusText,
+      status: res.status,
+    };
+    throw errorMessage
+  }
   const data = await res.json();
   return data.vans;
 }
