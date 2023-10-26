841413f0624fe9de7d0242be07943157030c06f3
Denis1982-11-23 <denisemelanov53635@gmail.com>
2023-10-27 00:18:02 +0500

    add main props

--- new_project/src/App.jsx
+++ new_project/src/App.jsx
@@ -1,16 +1,29 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import Main from "./components/Main";
 
 function App() {
-  fetch(
-    "http://api.weatherapi.com/v1/current.json?key=7053581a85ad4febb92173004232510&q=Chelyabinsk&aqi=no"
-  )
-    .then((data) => data.json())
-    .then((data) => console.log(data));
+  const [currentWeather, setCurrentWeather] = useState(false);
 
+  useEffect(() => {
+    fetch(
+      "http://api.weatherapi.com/v1/current.json?key=7053581a85ad4febb92173004232510&q=Chelyabinsk&aqi=no"
+    )
+      .then((data) => data.json())
+      .then((data) => {
+        // console.log(data);
+        setTimeout(() => {
+          setCurrentWeather(data.current);
+        }, 4000);
+      });
+  }, []);
   return (
     <div className="App">
-      <Main />
+      {currentWeather ? (
+        <Main props={currentWeather.condition} />
+      ) : (
+        "Loading..."
+      )}
     </div>
   );
 }
--- new_project/src/components/Main.jsx
+++ new_project/src/components/Main.jsx
@@ -1,7 +1,11 @@
-
-
-function Main() {
-  return <div className="main"></div>;
+function Main({props}) {
+  console.log(props)
+  return (
+    <div className="main">
+      <div className="condition">{props.text}</div>
+      <div className="location-info">{props.icon}</div>
+    </div>
+  );
 }
 
 export default Main;
