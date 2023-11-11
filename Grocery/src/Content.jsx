2ac4aa8f1900d5b9c09a1121b1fa896c9cfed102
Denis1982-11-23 <denisemelanov53635@gmail.com>
2023-11-11 08:06:57 +0500

    add check logic

--- Grocery/src/Content.jsx
+++ Grocery/src/Content.jsx
@@ -5,28 +5,45 @@ const Content = () => {
   const [items, setItems] = useState([
     {
       id: 1,
-      checked: false,
+      checked: true,
       item: "One half pound bag of Cocoa Covered Almonds Unsalted",
     },
     {
       id: 2,
-      checked: false,
+      checked: true,
       item: "Item2",
     },
     {
       id: 3,
-      checked: false,
+      checked: true,
       item: "Item3",
     },
   ]);
+
+  const handleCheck = (id) => {
+    const listItems = items.map((item) =>
+      item.id === id ? { ...item, checked: !item.checked } : item
+    );
+    setItems(listItems);
+    localStorage.setItem("shoppingList", JSON.stringify(listItems));
+  };
 
   return (
     <main>
       <ul>
         {items.map((item) => (
           <li className="item" key={item.id}>
-            <input type="checkbox" checked={item.checked} />
-            <label>{item.item}</label>
+            <input
+              onChange={() => handleCheck(item.id)}
+              type="checkbox"
+              checked={item.checked}
+            />
+            <label
+              style={item.checked ? { textDecoration: "line-through" } : null}
+              onDoubleClick={() => handleCheck(item.id)}
+            >
+              {item.item}
+            </label>
             <FaTrashAlt role="button" tabIndex="0" />
           </li>
         ))}
