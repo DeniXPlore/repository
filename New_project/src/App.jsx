1a80c56f4dba90dccfcb0516dc804f333dd8bf14
Denis1982-11-23 <denisemelanov53635@gmail.com>
2023-12-07 23:43:57 +0500

    add book component/styles

--- New_project/src/App.jsx
+++ New_project/src/App.jsx
@@ -1,12 +1,32 @@
-
 function App() {
-  
-
   return (
-    <>
-     
-    </>
-  )
+    <section className="bookList">
+      <Book />      
+      <Book />      
+      <Book />      
+      <Book />      
+    </section>
+  );
 }
 
-export default App
+const Book = () => {
+  return (
+    <article className="bookList">
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
+
+const Image = () => {
+  return <img src="./images/book-1.jpg" alt="Interesting Facts For Curious Minds"/>;
+};
+const Title = () => {
+  return <h2>Interesting Facts For Curious Minds</h2>;
+};
+const Author = () => {
+  return <h4>Jordan Moore</h4>;
+};
+
+export default App;
