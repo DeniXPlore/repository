61d6a18804dca69c8525cff5db79a751b4be9d2a
Denis1982-11-23 <denisemelanov53635@gmail.com>
2023-12-08 23:47:37 +0500

    made separate components

--- New_project/src/App.jsx
+++ New_project/src/App.jsx
@@ -1,32 +1,14 @@
+import { books } from "./Books";
+import { Book } from "./Book";
+
 function App() {
   return (
     <section className="bookList">
-      <Book />      
-      <Book />      
-      <Book />      
-      <Book />      
+      {books.map((book, index) => {
+        return <Book {...book} key={book.id} number={index}/>;
+      })}
     </section>
   );
 }
-
-const Book = () => {
-  return (
-    <article className="bookList">
-      <Image />
-      <Title />
-      <Author />
-    </article>
-  );
-};
-
-const Image = () => {
-  return <img src="./images/book-1.jpg" alt="Interesting Facts For Curious Minds"/>;
-};
-const Title = () => {
-  return <h2>Interesting Facts For Curious Minds</h2>;
-};
-const Author = () => {
-  return <h4>Jordan Moore</h4>;
-};
 
 export default App;
