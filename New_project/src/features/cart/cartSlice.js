3af9128a0b331a921f1f3705b79a5fd6da3ad69a
Denis1982-11-23 <denisemelanov53635@gmail.com>
2023-12-24 23:36:35 +0500

    made CartContainer and CartItem components/reducers

--- New_project/src/features/cart/cartSlice.js
+++ New_project/src/features/cart/cartSlice.js
@@ -1,17 +1,26 @@
 import { createSlice } from "@reduxjs/toolkit";
+import cartItems from "../../CartItems";
 
 const initialState = {
-  cartItems: [],
-  amount: 0,
+  cartItems: cartItems,
+  amount: 4,
   total: 0,
-  isLoading: true, 
-}
+  isLoading: true,
+};
 
 const cartSlice = createSlice({
-  name: 'cart',
-  initialState
-})
+  name: "cart",
+  initialState,
+  reducers: {
+    clearCart: (state) => {
+      state.cartItems = [];
+    },
+    removeItem: (state, action) => {
+      const itemId = action.payload;
+      state.cartItems = state.cartItems.filter((item) => item.id !== itemId);
+    },
+  },
+});
 
-// console.log(cartSlice);
-
-export default cartSlice.reducer
+export const { clearCart, removeItem } = cartSlice.actions;
+export default cartSlice.reducer;
