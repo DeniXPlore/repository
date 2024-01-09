2d38ed34e6d5d5b719f80416cf5d2e5949c23374
Denis1982-11-23 <denisemelanov53635@gmail.com>
2024-01-09 23:14:22 +0500

    started new_project

---
+++ new_project/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom"
+
+const Header = () => {
+  return (
+    <div className="styles-header">
+      <div className="styles-logo">
+        <Link to="/">
+          <img src="" alt="home" />
+        </Link>
+      </div>
+      </div>
+  )
+}
+export default Header
