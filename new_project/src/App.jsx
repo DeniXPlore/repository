bdc966abf7b3a4374f0c248a83ac0c0aa283c7d8
Denis1982-11-23 <denisemelanov53635@gmail.com>
2024-01-10 23:25:21 +0500

    made header

--- new_project/src/App.jsx
+++ new_project/src/App.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom'
 import Layout from './components/Layout'
 import Home from './features/home/home'
+import Cart from './features/cart/Cart'
 
 function App() {
   
@@ -8,6 +9,7 @@ function App() {
     <Routes>
       <Route path='/' element={<Layout/>}>
         <Route index element={<Home/>}/>
+        <Route path='/cart' element={<Cart/>}/>
       </Route>
 
     </Routes> 
--- new_project/src/components/Header.jsx
+++ new_project/src/components/Header.jsx
@@ -1,14 +1,60 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import logo from "../images/logo.svg";
+import avatar from "../images/avatar.jpg";
 
 const Header = () => {
   return (
     <div className="styles-header">
       <div className="styles-logo">
         <Link to="/">
-          <img src="" alt="home" />
+          <img src={logo} alt="home" />
         </Link>
       </div>
+      <div className="styles-info">
+        <div className="styles-user">
+          <div
+            className="styles-avatar"
+            style={{ backgroundImage: `url(${avatar})` }}
+          />
+          <div className="styles-username">Guest</div>
+        </div>
+        <form className="styles-form">
+          <div className="styles-icon">
+            <svg className="icon">
+              <use
+                xlinkHref={`${import.meta.env.PUBLIC_URL}/sprite.svg#search`}
+              />
+            </svg>
+          </div>
+          <div className="styles-input">
+            <input
+              type="search"
+              name="search"
+              placeholder="Search for anything..."
+              autoCapitalize="off"
+              onChange={() => {}}
+              value=""
+            />
+          </div>
+         {false && <div className="styles-box"></div>}
+        </form>
+        <div className="styles-account">
+          <Link to="/" className="styles-favorites">
+            <svg className="styles-icon-fav">
+              <use
+                xlinkHref={`${import.meta.env.PUBLIC_URL}/sprite.svg#heart`}
+              />
+            </svg>
+          </Link>
+          <Link to="/cart" className="styles-cart">
+            <svg className="styles-icon-cart">
+              <use xlinkHref={`${import.meta.env.PUBLIC_URL}/sprite.svg#bag`} />
+            </svg>
+            <span className="styles-count">2</span>
+          </Link>
+        </div>
       </div>
-  )
-}
-export default Header
+    </div>
+  );
+};
+export default Header;
