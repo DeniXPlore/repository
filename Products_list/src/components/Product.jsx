059069f26e57b84da3f0046be512b36cb073b36c
Denis1982-11-23 <denisemelanov53635@gmail.com>
2024-02-05 23:30:43 +0500

    made Card

--- Products_list/src/components/Product.jsx
+++ Products_list/src/components/Product.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react"
+import { Card, Button } from "react-bootstrap"
 
 const Product = () => {
   
@@ -8,11 +9,30 @@ const Product = () => {
     fetch('https://fakestoreapi.com/products')
     .then(data => data.json())
     .then(result => getProduts(result))
-  }, [])
+      }, [])
+
+  const cards = products.map(product => (
+    <div className="col-md-3">
+      <Card style={{ width: '18rem', margin: '2rem' }}>
+      <Card.Img variant="top" src={product.image} style={{width: '100px', height: '130px', margin: '1rem 0 0 1rem '}} />
+      <Card.Body>
+        <Card.Title>{product.title}</Card.Title>
+        <Card.Text>
+         {product.description}
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
+    </div>
+  ))
+ console.log(products)
   return (
     <>
     <h1>Product dashboard</h1>
-    {JSON.stringify(products)}
+    <div className="row">
+      {cards}
+      {/* {JSON.stringify(products)} */}
+    </div>
     </>
   )
 }
