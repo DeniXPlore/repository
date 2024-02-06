b97c4520f413ffffdc18e6efbf1793751236e680
Denis1982-11-23 <denisemelanov53635@gmail.com>
2024-02-06 23:35:17 +0500

    made components/routes

--- Products_list/src/components/Product.jsx
+++ Products_list/src/components/Product.jsx
@@ -1,39 +1,45 @@
-import { useState, useEffect } from "react"
-import { Card, Button } from "react-bootstrap"
+import { useState, useEffect } from "react";
+import { Card, Button } from "react-bootstrap";
 
 const Product = () => {
-  
-  const [products, getProduts] = useState([])
+  const [products, getProduts] = useState([]);
 
   useEffect(() => {
-    fetch('https://fakestoreapi.com/products')
-    .then(data => data.json())
-    .then(result => getProduts(result))
-      }, [])
+    fetch("https://fakestoreapi.com/products")
+      .then((data) => data.json())
+      .then((result) => getProduts(result));
+  }, []);
 
-  const cards = products.map(product => (
-    <div className="col-md-3">
-      <Card style={{ width: '18rem', margin: '2rem' }}>
-      <Card.Img variant="top" src={product.image} style={{width: '100px', height: '130px', margin: '1rem 0 0 1rem '}} />
-      <Card.Body>
-        <Card.Title>{product.title}</Card.Title>
-        <Card.Text>
-         {product.description}
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
+  const cards = products.map((product) => (
+    <div className="col-md-3" style={{ marginBottom: "1rem" }}>
+      <Card key={products.id} className="h-100">
+        <div className="text-center">
+          <Card.Img
+            variant="top"
+            src={product.image}
+            style={{
+              width: "100px",
+              height: "130px",
+              marginTop: "1rem",
+            }}
+          />
+        </div>
+        <Card.Body>
+          <Card.Title>{product.title}</Card.Title>
+          <Card.Text>{product.description}</Card.Text>
+        </Card.Body>
+        <Card.Footer style={{background: 'white'}}>
+          <Button variant="primary">Add to cart</Button>
+        </Card.Footer>
+      </Card>
     </div>
-  ))
- console.log(products)
+  ));
+  console.log(products);
   return (
     <>
-    <h1>Product dashboard</h1>
-    <div className="row">
-      {cards}
-      {/* {JSON.stringify(products)} */}
-    </div>
+      <h1>Product dashboard</h1>
+      <div className="row">{cards}</div>
     </>
-  )
-}
-export default Product
+  );
+};
+export default Product;
