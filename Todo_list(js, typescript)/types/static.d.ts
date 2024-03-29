b568c33f3566a10daab35a0e53be67cc185cd6d0
Denis1982-11-23 <denisemelanov53635@gmail.com>
2024-03-29 23:23:07 +0500

    comments

--- Todo_list(js, typescript)/types/static.d.ts
+++ Todo_list(js, typescript)/types/static.d.ts
@@ -1,7 +1,4 @@
-/* Use this file to declare any custom file extensions for importing */
-/* Use this folder to also add/extend a package d.ts file, if needed. */
 
-/* CSS MODULES */
 declare module '*.module.css' {
   const classes: { [key: string]: string };
   export default classes;
@@ -56,4 +53,4 @@ declare module '*.png' {
   export default ref;
 }
 
-/* CUSTOM: ADD YOUR OWN HERE */
+
