2ef75228d4d6fafc4ef7ba199426eab04077cd0f
Denis1982-11-23 <denisemelanov53635@gmail.com>
2024-05-02 23:09:01 +0500

    made drawing component

--- Hangman(react, ts)/src/App.tsx
+++ Hangman(react, ts)/src/App.tsx
@@ -1,16 +1,34 @@
-import { useState } from "react"
-import words from './wordList.json'
+import { useState } from "react";
+import words from "./wordList.json";
+import HangManDrawing from "./HangManDrawing";
+import HangmanWord from "./HangmanWord";
+import Keyboard from "./Keyboard";
 
-function App() {  
+function App() {
   const [wordToGuess, setWordToGuess] = useState(() => {
-    return words[Math.floor(Math.random() * words.length)]
-  }) 
+    return words[Math.floor(Math.random() * words.length)];
+  });
+  const [guessedLetters, setGuessedLetters] = useState<string>([]);
 
   return (
     <>
-      <h1>hi</h1>
+      <div
+        style={{
+          maxWidth: "800px",
+          display: "flex",
+          flexDirection: "column",
+          gap: "2rem",
+          margin: "0 auto",
+          alignItems: "center",
+        }}
+      >
+        <div style={{fontSize: '2rem', textAlign: 'center'}}>Lose Win</div>
+        <HangManDrawing/>
+        <HangmanWord/>
+        <Keyboard/>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
