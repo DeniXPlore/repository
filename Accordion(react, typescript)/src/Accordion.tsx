a9064606954268ddbd7c9a3fcde5e45511616552
Denis1982-11-23 <denisemelanov53635@gmail.com>
2024-07-27 07:41:33 +0500

    map data

---
+++ Accordion(react, typescript)/src/Accordion.tsx
@@ -0,0 +1,26 @@
+import { useState } from "react"
+import data from "./data.ts"
+
+const
+const Accordion = () => {
+
+  const [selected, setSelected] = useState(null)
+
+  return (
+    <div className="wrapper">
+      <div className="accordion">
+        {data & data.length > 0 
+        ? data.map(dataItem => <div className="item">
+          <div className="title">
+            <h3>{dataItem.question}</h3>
+            <span>+</span>
+            </div>
+        </div>)
+        : <div>No Data Found</div>
+        
+        }
+      </div>
+    </div>
+  )
+}
+export default Accordion
