961a10f18db85e6e8f250028842691183be2847e
Denis1982-11-23 <denisemelanov53635@gmail.com>
2025-05-02 23:26:12 +0500

    routes, layouts

--- userlist-next/app/page.tsx
+++ userlist-next/app/page.tsx
@@ -1,13 +1,15 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 import { Inter } from 'next/font/google'
+import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main className={styles.main}>
+    <main >
      <h1> Hello World</h1>
+     <Link href="/about">Go to About page</Link>
     </main>
   )
 }
