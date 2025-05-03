62dad7fd683091ee039cbe71cd3069e2b5afe1c2
Denis1982-11-23 <denisemelanov53635@gmail.com>
2025-05-03 23:38:03 +0500

    fetching

--- userlist-next/app/page.tsx
+++ userlist-next/app/page.tsx
@@ -1,15 +1,16 @@
-import Image from 'next/image'
-import styles from './page.module.css'
-import { Inter } from 'next/font/google'
-import Link from 'next/link'
+import { Inter } from "next/font/google";
+import styles from "./page.module.css";
+import Link from "next/link";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main >
-     <h1> Hello World</h1>
-     <Link href="/about">Go to About page</Link>
+    <main>
+      <h1>Home Page</h1>
+      <p>
+        <Link href="/users">Users</Link> 
+      </p>
     </main>
-  )
+  );
 }
